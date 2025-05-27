497a8b677c68f0fefb6eb4bc5440a55fc80fe733
LucasLouis62 <lucas.louis@edu.ece.fr>
2025-05-27 11:36:25 +0200

    Adapt to Bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Agora-Shop---WebDynamique
+
+Ya beaucoup de chose dans prime.css à supprimer après l'adapatation à bootstrap
--- acceuil.js
+++ acceuil.js
@@ -5,19 +5,19 @@ $(document).ready(function () {
     var i = 0;
 
     // Affiche uniquement la première image
-    $img.hide().eq(i).show();
+    $img.removeClass('active').eq(i).addClass('active');
 
     // Bouton suivant
     $carrousel.find('.next').click(function () {
-        $img.eq(i).hide();
+        $img.eq(i).removeClass('active');
         i = (i + 1) > indexImg ? 0 : i + 1;
-        $img.eq(i).show();
+        $img.eq(i).addClass('active');
     });
 
     // Bouton précédent
     $carrousel.find('.prev').click(function () {
-        $img.eq(i).hide();
+        $img.eq(i).removeClass('active');
         i = (i - 1) < 0 ? indexImg : i - 1;
-        $img.eq(i).show();
+        $img.eq(i).addClass('active');
     });
 });
